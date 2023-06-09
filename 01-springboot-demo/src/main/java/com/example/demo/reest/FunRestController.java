98f4a218f6d8fc06ac83b5bce797e7b4307229bf
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2023-06-09 17:20:26 +0900

    init

---
+++ 01-springboot-demo/src/main/java/com/example/demo/reest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.reest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+
+        @GetMapping("/")
+        public String sayHello(){
+            return "Hello World";
+        }
+
+}
