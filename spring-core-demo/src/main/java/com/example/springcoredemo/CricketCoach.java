8a00309d8930787563ce0de7fd44263cacee1891
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2023-06-13 23:45:10 +0900

    coach

---
+++ spring-core-demo/src/main/java/com/example/springcoredemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.example.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component //이제부터 이 클래스는 java bean이다.
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes wwww";
+    }
+}
