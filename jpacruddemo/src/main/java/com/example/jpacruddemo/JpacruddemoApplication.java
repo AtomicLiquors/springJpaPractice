9c810da75d308299302fed60864140588efcc592
chb <gyqls234@gmail.com>
2023-06-30 07:36:18 +0900

    crud complete

--- jpacruddemo/src/main/java/com/example/jpacruddemo/JpacruddemoApplication.java
+++ jpacruddemo/src/main/java/com/example/jpacruddemo/JpacruddemoApplication.java
@@ -31,10 +31,16 @@ public class JpacruddemoApplication {
 			//queryForStudents(studentDAO);
 			//queryForStudentsByLastName(studentDAO);
 			//updateStudent(studentDAO);
-			deleteStudent(studentDAO );
+			//deleteStudent(studentDAO );
+			deleteAllStudents(studentDAO);
 		};
 	}
 
+	private void deleteAllStudents(StudentDAO studentDAO){
+		System.out.println("Deleting all students");
+		int numRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted row count : " + numRowsDeleted);
+	}
 
 	private void deleteStudent(StudentDAO studentDAO){
 		int studentId = 5;
