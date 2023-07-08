ca2ba55f6e23875642f3a436a8c8924be5836630
chb <gyqls234@gmail.com>
2023-07-08 18:06:24 +0900

    onetoone crud complete

--- jpa-advanced-mapping/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ jpa-advanced-mapping/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,9 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.hibernate.boot.jaxb.hbm.internal.GenerationTimingConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +16,41 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
   	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		//Executed after the Spring Beans have been loaded.
 		return runner -> {
-			System.out.println("1kooong2");
+//			createInstructor(appDAO);
+			//findInstructor(appDAO);
+			deleteInstructor(appDAO);
 		};
+	}
+
+private void deleteInstructor(AppDAO appDAO)	{
+		int theId = 2;
+	System.out.println("Deleting instructor id: " + theId);
+	appDAO.deleteInstructor(theId);
+	System.out.println("Deleted!");
+}
+private void findInstructor(AppDAO appDAO) {
+
+	int theId = 1;
+	System.out.println("Finding instructor id : " + theId);
+	Instructor tempInstructor = appDAO.findInstructorById(theId);
+	System.out.println("tempInstructor: " + tempInstructor);
+	System.out.println("the associate instructorDetail only : " + tempInstructor.getInstructorDetail());
+
+}
+	private void createInstructor(AppDAO appDAO){
+		Instructor tempInstructor = new Instructor("Morith", "Vega", "gyqls234@gmail.com");
+
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.fixer.com/youtube","Guns for hire!!!");
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor:" + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Complete!");
 	}
 }
