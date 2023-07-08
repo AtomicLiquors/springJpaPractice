26bf316ff720e7706939560b0098a845f375feab
chb <gyqls234@gmail.com>
2023-07-08 16:57:03 +0900

    Entity setting

--- jpa-advanced-mapping/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ jpa-advanced-mapping/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +11,11 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
-
+  	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		//Executed after the Spring Beans have been loaded.
+		return runner -> {
+			System.out.println("1kooong2");
+		};
+	}
 }
